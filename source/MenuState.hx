b4eb0a573f118b01e9b0cf7e6069dd2e3306fb2f
Asplomer <asplomer@gmail.com>
2016-08-31 12:10:19 -0300

    Add files via upload

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -12,10 +12,44 @@ class MenuState extends FlxState
 	override public function create():Void
 	{
 		super.create();
+		var intro:FlxSprite = new FlxSprite();
+
+		flixel.FlxG.resizeGame(480,432);
+		intro.loadGraphic(AssetPaths.IntroA__png);
+		add(intro);
+		var score:Int = 0;
+		var hiscore:Int = 0;
+
+		var timer:Int = 0;
+		
+		
+		
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+		
+		//var menu:FlxState = new FlxState();
+		//flixel.FlxG.switchState(menu);
+		if (timer >100){}
+		else(){};
+		if(FlxG.keys.pressed.ENTER)
+		{
+			var Invasion:FlxState = new FlxState();
+			flixel.FlxG.switchState(Invasion);
+		}
+		if (FlxG.keys.pressed.ENTER) 
+		{
+			
+			
+		}
+		
+		if (FlxG.keys.pressed.BACKSPACE)
+		{
+			Sys.exit(0);
+		}
+		
+		
 	}
 }
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -12,10 +12,20 @@ class PlayState extends FlxState
 	override public function create():Void
 	{
 		super.create();
+		var invasion:FlxState = new FlxState();
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+		if (FlxG.keys.pressed.SPACE)
+		{
+			//shoot
+		}
 	}
+	/*
+		shoot.velocity.x=200;
+		
+	
+	*/
 }
